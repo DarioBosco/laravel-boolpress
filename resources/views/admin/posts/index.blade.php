661ae7a7dd2da2905fd71c2a1bd4af7a7d2efb43
DarioBosco <dario_bosco@protonmail.com>
2021-11-05 15:56:53 +0100

    Aggiunta view per /dashboard/posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,13 +1,32 @@
-
 @extends('layouts.dashboard')
 
 @section('content')
 
-<ul>
-    @foreach ($posts as $post)
-    <li>
-        @dump($post)
-    </li>
-    @endforeach
-</ul>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author</th>
+            <th scope="col">Published</th>
+            <th scope="col">Controls</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+        <tr>
+            <td>{{$post->id}}</td>
+            <td>{{$post->title}}</td>
+            <td>{{$post->author}}</td>
+            <td>{{($post->published) ? 'true' : 'false' }}</td>
+            <td>
+                <a class="btn btn-primary" href="">Details</a>
+                <a class="btn btn-warning" href="">Modify</a>
+                <a class="btn btn-danger" href="">Delete</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function() {
+Route::middleware('auth')->namespace('Admin')->prefix('dashboard')->name('admin.')->group(function() {
 
     Route::get('/', 'HomeController@index')->name('index');
-
     Route::resource('/posts', 'PostController');
 
 });
