f9a990d005008d9cd0f10059975398de6654d69d
DarioBosco <dario_bosco@protonmail.com>
2021-11-06 11:35:56 +0100

    Eliminato il frontoffice

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,24 @@ use \Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Auth::routes();
+//Landing Page
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('homepage');
 
-Auth::routes();
+/* //Front Office
+Route::get('/posts', 'PostController@index')->name('user-posts');
+Route::get('/posts/{slug}', 'PostController@show')->name('user-posts-detail');
+Route::resource('/posts', 'PostController'); */
 
+//Login confirmation page
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/posts', 'PostController@index')->name('index');
 
+//Back Office
 Route::middleware('auth')->namespace('Admin')->prefix('dashboard')->name('admin.')->group(function() {
-
+    
     Route::get('/', 'HomeController@index')->name('index');
     Route::resource('/posts', 'PostController');
-
+    
 });
