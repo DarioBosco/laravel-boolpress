46b66d48ddb72ec687d55b80ccce156e5370dc20
DarioBosco <dario_bosco@protonmail.com>
2021-11-08 10:27:57 +0100

    Modificato il seeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -19,7 +19,7 @@ class PostSeeder extends Seeder
             $new_post->title = $faker->words(5, true);
             $new_post->content = $faker->realText('800', 2);
             $new_post->author = $faker->name();
-            $new_post->slug = Str::kebab($new_post->title);
+            $new_post->slug = Str::slug($new_post->title);
             $new_post->created_at = now();
             $new_post->updated_at = now();
             $new_post->is_public = 1;
@@ -29,7 +29,7 @@ class PostSeeder extends Seeder
                 $new_post->title = $faker->words(5, true);
                 $new_post->content = $faker->realText('800', 2);
                 $new_post->author = $faker->name();
-                $new_post->slug = Str::kebab($new_post->title);
+                $new_post->slug = Str::slug($new_post->title);
                 $new_post->created_at = now();
                 $new_post->updated_at = now();
                 $new_post->is_public = 0;
