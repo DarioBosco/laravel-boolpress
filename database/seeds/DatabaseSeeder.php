6bc5598935a0f5dff1779b2eef84ee5b20f96cbf
DarioBosco <dario_bosco@protonmail.com>
2021-11-10 14:52:52 +0100

    Aggiunti migrations/controlles/models per i tag

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PostSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
